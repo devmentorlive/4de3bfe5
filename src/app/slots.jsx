2823c91938d14ca062515122c5517eb27866851d
Mark Tellez <mark.tellez@icloud.com>
2020-08-12 01:20:32 -0500

    12h formatting

--- src/app/slots.jsx
+++ src/app/slots.jsx
@@ -3,10 +3,14 @@ import React from "react";
 export default function Slots({ days, selectedDay }) {
   const blocks = days[selectedDay];
 
+  function get12h(hour) {
+    return hour < 13 ? `${hour}am` : `${hour - 12}pm`;
+  }
+
   function getSlots(block) {
     const slots = [];
     for (let i = block.from; i < block.to; i++) {
-      slots.push(i);
+      slots.push(`${get12h(i)} - ${get12h(i + 1)}`);
     }
 
     return slots;
