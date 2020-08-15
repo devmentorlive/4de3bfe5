b8a40a9ee1389a394671a068a0c51c002ba01b00
Mark Tellez <mark.tellez@icloud.com>
2020-08-14 22:36:36 -0500

    refactor

--- src/app/time-slots/index.jsx
+++ src/app/time-slots/index.jsx
@@ -1,5 +1,17 @@
 import React from "react";
-import Range from "./range";
+
+function getSlots(block) {
+  const slots = [];
+  for (let i = block.from; i < block.to; i++) {
+    slots.push(`${get12h(i)} - ${get12h(i + 1)}`);
+  }
+
+  return slots;
+}
+
+function get12h(hour) {
+  return hour < 13 ? `${hour}am` : `${hour - 12}pm`;
+}
 
 export default function Slots({
   schedule,
@@ -7,17 +19,25 @@ export default function Slots({
   selectedSlot,
   setSelectedSlot,
 }) {
+  const blocks = schedule[selectedDay];
   return selectedDay ? (
-    <div className="slots">
-      <ul className="slots">
-        <Range
-          schedule={schedule}
-          selectedDay={selectedDay}
-          selectedSlot={selectedSlot}
-          setSelectedSlot={setSelectedSlot}
-        />
-      </ul>
-    </div>
+    <ul className="slots">
+      {blocks && blocks.length > 0
+        ? blocks.map((block) =>
+            getSlots(block).map((range) => (
+              <li
+                key={range}
+                onClick={() => setSelectedSlot(range)}
+                onKeyPress={() => setSelectedSlot(range)}
+                className={`pill ${selectedSlot === range ? "selected" : null}`}
+                tabIndex={0}
+              >
+                {range}
+              </li>
+            ))
+          )
+        : "There are no slots available that day"}
+    </ul>
   ) : (
     <></>
   );
